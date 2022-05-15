6b8d5031ef508bb409953e30fe4de6bae14cec10
gilang_gans <gilangsos99@gmail.com>
2022-05-15 15:07:56 +0700

    Tugas Js 9
    Tugas ini untuk niomic

---
+++ javascript/tugas9.js
@@ -0,0 +1,12 @@
+function niomic(){
+  var dataKuliahan = {
+    nama: "gilang romadhon",
+    jurusan : "teknik informatika",
+    semester : "7"
+  }
+  for (var x in dataKuliahan){
+    console.log(dataKuliahan)
+  }
+}
+
+niomic();
